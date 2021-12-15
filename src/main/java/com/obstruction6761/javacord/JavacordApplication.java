3ce602ceee1a222fa87510d4a63aede08d68eae9
obstruction6761 <obstruction6761@gmail.com>
2021-12-15 16:03:54 -0700

    spring initializr

---
+++ src/main/java/com/obstruction6761/javacord/JavacordApplication.java
@@ -0,0 +1,13 @@
+package com.obstruction6761.javacord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obstruction6761/javacord/JavacordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obstruction6761.javacord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
